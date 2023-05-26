c1dff63ec5ec886d839697d65f45e6cbfbab2c7e
alsfkl222 <alskfl222@kakao.com>
2023-05-26 13:42:42 +0900

    FIX: sessionStorage for isLogin

--- util/store.ts
+++ util/store.ts
@@ -7,15 +7,15 @@ interface Store {
 }
 
 export const useStore = create<Store>((set) => ({
-  isLogin: JSON.parse(localStorage.getItem('isLogin') || 'false'),
+  isLogin: JSON.parse(sessionStorage.getItem('isLogin') || 'false'),
   signIn: () =>
     set((state) => {
-      localStorage.setItem('isLogin', 'true');
+      sessionStorage.setItem('isLogin', 'true');
       return { ...state, isLogin: true };
     }),
   signOut: () =>
     set((state) => {
-      localStorage.setItem('isLogin', 'false');
+      sessionStorage.setItem('isLogin', 'false');
       return { ...state, isLogin: false };
     }),
 }));

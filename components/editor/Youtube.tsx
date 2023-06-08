762a3209dfa6e58993cfba048f217aeedf99f701
alsfkl222 <alskfl222@kakao.com>
2023-06-08 18:26:58 +0900

    FIX: editor youtube embedding add 1 line

--- components/editor/Youtube.tsx
+++ components/editor/Youtube.tsx
@@ -74,7 +74,7 @@ export const createYoutubeToolbarIcon =
         .getInstance()
         .setHTML(
           existHTML +
-            `<div><iframe width="560" height="315" src="https://www.youtube.com/embed/${youtubeId}" title="Youtube player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br></div>`
+            `<div><iframe width="560" height="315" src="https://www.youtube.com/embed/${youtubeId}" title="Youtube player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br></div><p></p>`
         );
       ytInput.value = '';
     };
